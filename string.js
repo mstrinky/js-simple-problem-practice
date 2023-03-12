ab19a03d24101377398f5c301ee402bd5cacfc30
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-12 23:57:52 +0600

    about of string

--- string.js
+++ string.js
@@ -2,9 +2,9 @@ var schoolName= 'Islam nagar high school';
 var searchOneWord = schoolName.includes("nagar");
 // to find out item 
 console.log(searchOneWord);
-// to find out item index 
+// to find out item index ------------
 console.log(schoolName.indexOf('high'));
-// to find out the first entry of string 
+// to find out the first entry of string -------------
 console.log(schoolName.startsWith('Islam'));
-// to find out  the end entry of String 
+// to find out  the end entry of String ------------
 console.log(schoolName.endsWith('school'));
